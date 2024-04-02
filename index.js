7890ab404661016b2bc23d7c036d04e4a4afe5ba
Indra032001 <144004852+Indra032001@users.noreply.github.com>
2024-04-02 16:12:52 +0530

    Add files via upload
    This helps you to know me

---
+++ index.js
@@ -0,0 +1,16 @@
+let aboutlinks = document.getElementsByClassName('about-link');
+let abouttabcontents = document.getElementsByClassName('about-tab-content');
+function openTabContent(tabName)
+{
+  for(aboutlink of aboutlinks)
+  {
+    aboutlink.classList.remove("active-link");
+  }
+  //this is for link-content
+  for(abouttabcontent of abouttabcontents)
+  {
+    abouttabcontent.classList.remove("active-content");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-content");
+}
